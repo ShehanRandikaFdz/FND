0c0a6aa2d0f633599692c7a9e1e9effc5a35164e
Shashindi Harischandra <shashindhieravishka@gmail.com>
2025-10-03 20:21:13 +0530

    Updating sytem

--- integrated_system.py
+++ integrated_system.py
@@ -13,7 +13,7 @@ from typing import Dict, Any, List
 import json
 
 # Import existing model predictor
-from app_final import ModelPredictor
+from app import ModelPredictor
 
 # Import verdict agent
 from verdict_agent import VerdictAgent, ModelResult, VerdictType, ConfidenceLevel
